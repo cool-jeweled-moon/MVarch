824524b615367553cc781d438d1098423990af15
BorisAneli <boris@aita.travel>
2025-04-03 18:50:29 +0700

    Initial Commit

---
+++ MVarch/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MVarch
+//
+//  Created by Борис Анели on 03.04.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
